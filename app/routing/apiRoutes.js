bbb94c26cfd6e4f47d2ec91ea3c2c1390e78e97e
Donald Blanc <dxb3662@rit.edu>
2018-05-17 13:00:29 -0400

    heroku Deployment

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,7 +10,42 @@ module.exports = function (app) {
 
     app.post("/api/friends", function(req,res) {
 
-    })
+        var  match = {
+            name:"",
+            photo: "",
+            fdifference: 1000
+        };
+
+        console.log(req.body);
+
+        var userResponses = req.body;
+        var userScores = userResponses.scores;
+
+        console.log(userScores);
+
+        var totalDifference = 0;
+
+        var length = friends.length;
+        for( var i =0; i < length; i++ ) {
+            console.log(friends[i]);
+            totalDifference =0
+
+                for(var j =0; j < friends[i].scores[i]; j++){
+                    totalDifference += Math.abs( parseInt(userScores[j]) - parseInt(friends[i].scores[j]))
+
+                    if( totalDifference <= match.fdifference ){
+                        match.name = friends[i].name;
+                        match.photo = friends[i].photo;
+                        match.fdifference = totalDifference;
+
+                    }
+                }
+        }// for
+
+        friends.push(userResponses);
+
+        res.json(match);
+    })// post method
 
 
 
